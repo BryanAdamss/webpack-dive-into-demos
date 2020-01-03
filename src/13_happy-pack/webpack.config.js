2bc09319631ea8998a1c2e2679cdc2065a3c2a3a
BryanAdamss <BryanAdamss@foxmail.com>
2020-01-03 10:23:33 +0800

    feat(13): 优化 - happypack

---
+++ src/13_happy-pack/webpack.config.js
@@ -0,0 +1,44 @@
+// HappyPack可以让 Webpack 同一时刻处理多个任务，发挥多核 CPU 电脑的威力，以提升构建速度
+// 由于HappyPack 对file-loader、url-loader 支持的不友好，所以不建议对该loader使用。
+
+const path = require('path')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const HappyPack = require('happypack')
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        // 把对 .js 文件的处理转交给 id 为 babel 的 HappyPack 实例
+        use: ['happypack/loader?id=babel'],
+        // 排除 node_modules 目录下的文件，node_modules 目录下的文件都是采用的 ES5 语法，没必要再通过 Babel 去转换
+        exclude: path.resolve(__dirname, 'node_modules')
+      },
+      {
+        // 把对 .css 文件的处理转交给 id 为 css 的 HappyPack 实例
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          use: ['happypack/loader?id=css']
+        })
+      }
+    ]
+  },
+  plugins: [
+    new HappyPack({
+      // 用唯一的标识符 id 来代表当前的 HappyPack 是用来处理一类特定的文件
+      id: 'babel',
+      // 如何处理 .js 文件，用法和 Loader 配置中一样
+      loaders: ['babel-loader?cacheDirectory']
+      // ... 其它配置项
+    }),
+    new HappyPack({
+      id: 'css',
+      // 如何处理 .css 文件，用法和 Loader 配置中一样
+      loaders: ['css-loader']
+    }),
+    new ExtractTextPlugin({
+      filename: `[name].css`
+    })
+  ]
+}
